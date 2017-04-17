d8060d97e96849918fb1a978025d758f5619b048
Daichi Tamaki <tamaki.htm@gmail.com>
2017-04-18 00:37:47 +0900

    fix

--- sample/webpack/config.js
+++ sample/webpack/config.js
@@ -3,6 +3,6 @@ var webpack = require('webpack');
 module.exports = {
   output: {
     libraryTarget: 'var',
-    library: 'WebpackSample'
+    library: 'Module'
   }
 };
--- sample/webpack/index.js
+++ sample/webpack/index.js
@@ -1,6 +1,8 @@
+var jQuery = require('jQuery');
 var Buffer = require('buffer/').Buffer
 
 module.exports = {
+  jQuery: jQuery,
   encode: function (text) {
     return new Buffer(text).toString('base64');
   }
