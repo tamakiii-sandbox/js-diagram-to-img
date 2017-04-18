d7a81cfc7b0df61caaf70b6ce883a266cf0662c6
tamakiii <tamaki.htm@gmail.com>
2017-04-19 04:36:15 +0900

    fix dependency errors
    https://github.com/request/request/issues/1529#issuecomment-103454943

--- config.js
+++ config.js
@@ -1,8 +1,22 @@
-var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
   output: {
-    libraryTarget: 'var',
-    library: 'Module'
+    path: path.join(__dirname, 'scripts'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.json$/, loader: 'json-loader' }
+    ]
+  },
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.js']
+  },
+  node: {
+    console: true,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
   }
 };
