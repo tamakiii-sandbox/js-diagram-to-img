baaa69aa79b5deb71d4f829b664a6dcf8dd82d01
Daichi Tamaki <tamaki.htm@gmail.com>
2017-04-19 23:14:41 +0900

    doesn't work
    $ bin/cli.js
    /Users/tamaki/Sites/tamakiii/js-diagram-to-img/index.js:12
        console.log(window.$('#canvas').toDataURL('image/png'))
                                        ^
    TypeError: window.$(...).toDataURL is not a function
        at Object.done (/Users/tamaki/Sites/tamakiii/js-diagram-to-img/index.js:12:37)
        at process.nextTick (/Users/tamaki/Sites/tamakiii/js-diagram-to-img/node_modules/jsdom/lib/jsdom.js:325:18)
        at _combinedTickCallback (internal/process/next_tick.js:73:7)
        at process._tickCallback (internal/process/next_tick.js:104:9)

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var jsdom = require('jsdom')
+var fs = require('fs');
+
+var html = fs.readFileSync('./sample/chart.js/index.html', 'utf-8');
+var jQuery = require('jQuery');
 
 jsdom.env(
-  'https://iojs.org/dist/',
+  html,
+  // 'https://iojs.org/dist/',
   ['http://code.jquery.com/jquery.js'],
   function (err, window) {
-    console.log('There have been', window.$('a').length - 4, 'io.js releases!')
+    console.log(window.$('#canvas').toDataURL('image/png'))
+    // console.log('There have been', window.$('a').length - 4, 'io.js releases!')
   }
 );
 
