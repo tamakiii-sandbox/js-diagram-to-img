957441d0e53b40d8fa9b8113df11ba65fc6bca80
Daichi Tamaki <tamaki.htm@gmail.com>
2017-04-19 23:09:58 +0900

    jsdom test

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+var jsdom = require('jsdom')
 
-console.log('...')
+jsdom.env(
+  'https://iojs.org/dist/',
+  ['http://code.jquery.com/jquery.js'],
+  function (err, window) {
+    console.log('There have been', window.$('a').length - 4, 'io.js releases!')
+  }
+);
 
 module.exports.var = {}
